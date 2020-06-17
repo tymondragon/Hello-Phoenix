fb861040d2144c8ebd97f788a6d9769a81400c31
Ty Mondragon <tymondragon@gmail.com>
2020-06-17 14:21:28 -0600

    map Parent:User to Child:Credential with <belongs_to>

--- lib/hello/accounts/credential.ex
+++ lib/hello/accounts/credential.ex
@@ -1,11 +1,12 @@
 defmodule Hello.Accounts.Credential do
   use Ecto.Schema
   import Ecto.Changeset
-
+  alias Hello.Accounts.User
+  
   schema "credentials" do
     field :email, :string
     field :user_id, :id
-
+    belongs_to :user, User
     timestamps()
   end
 
